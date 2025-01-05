5a4011b1770a081498f3f0b12fde24672900cb07
Guilherme <1190640@isep.ipp.pt>
2025-01-05 10:53:48 +0000

    implementing different environments

--- P2/docker-swarm/build.bat
+++ P2/docker-swarm/build.bat
@@ -1,19 +1,27 @@
+@echo off
+
 REM Load environment variables from config.bat
 call config.bat
 
-REM Build Docker images
-start /B docker-compose -f boot-services.yml -p boot-services build
-start /B docker-compose -f populate-databases.yml -p populate-databases build
-start /B docker-compose -f books-query.yml -p books-query build
-start /B docker-compose -f books-command.yml -p books-command build
+REM Define environment file path
+REM production.env / testing.env / development.env
+set ENV_FILE=development.env
+
+REM Build Docker images with environment variables
+start /B docker-compose --env-file %ENV_FILE% -f boot-services.yml -p boot-services build
+start /B docker-compose --env-file %ENV_FILE% -f populate-databases.yml -p populate-databases build
+start /B docker-compose --env-file %ENV_FILE% -f books-query.yml -p books-query build
+start /B docker-compose --env-file %ENV_FILE% -f books-command.yml -p books-command build
 REM todo add more services here
-start /B docker-compose -f after-services.yml -p after-services build
+start /B docker-compose --env-file %ENV_FILE% -f after-services.yml -p after-services build
 
 REM Wait for all background processes to finish
-REM Note: Windows batch files do not have a built-in 'wait' command. We will use a workaround.
 :wait_loop
 tasklist | find /I "docker-compose.exe" >nul 2>&1
 if not errorlevel 1 (
     timeout /T 1 >nul
     goto wait_loop
 )
+
+@echo off
+echo All Docker Compose builds are complete.
